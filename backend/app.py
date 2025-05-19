67ee396d0515332b971e46353ad1b10b42f025dc
MahadSeecs <mahadm.bscs21seecs@seecs.edu.pk>
2025-05-19 10:28:49 +0500

    added local app
    rafay cute

---
+++ backend/app.py
@@ -0,0 +1,161 @@
+from flask import Flask, render_template, redirect, url_for, request, send_file, jsonify
+import os
+from PIL import Image, ImageChops
+from io import BytesIO
+import requests
+import numpy as np
+import sys
+import os
+import cv2
+import time
+
+def show_fullscreen(image_pil):
+    image_np = np.array(image_pil.convert("RGB"))  # Convert PIL to numpy array
+    image_cv = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)  # Convert RGB to BGR for OpenCV
+
+    cv2.namedWindow("Sketch", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Sketch", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.imshow("Sketch", image_cv)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def is_black_image(image):
+    # Compare with black image of the same size
+    black = Image.new("RGB", image.size)
+    diff = ImageChops.difference(image, black)
+    return not diff.getbbox()  # Returns None if the image is completely black
+
+# This gets the directory where app.py is located
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Add the root directory to the Python path
+sys.path.append(base_dir)
+model_path = os.path.join(base_dir, 'model', 'pytorch_CycleGAN_and_pix2pix', 'pytorch_CycleGAN_and_pix2pix')
+sys.path.append(model_path)
+
+from model.pytorch_CycleGAN_and_pix2pix.pytorch_CycleGAN_and_pix2pix.script import img_to_sketch
+
+# from mysqldb import mysql
+
+app = Flask(__name__, 
+            template_folder=os.path.join(os.pardir, "frontend"), 
+            static_folder=os.path.join(os.pardir, "frontend", "static"))
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fruity'
+app.config['MYSQL_DB'] = 'tracefy'
+
+# mysql.init_app(app)
+
+UPLOAD_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# COLAB_BACKEND_URL = "https://e629-34-142-198-91.ngrok-free.app/generate"
+COLAB_BACKEND_URL = "https://0754-35-237-61-68.ngrok-free.app/generate"
+# COLAB_BACKEND_URL = "https://eojwzooypjf31k5.m.pipedream.net"
+
+
+# Home route
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        return render_template("prompt.html")
+    
+    return render_template("index.html")
+
+    #     cursor = mysql.connection.cursor()
+    #     query = "SELECT * FROM accounts WHERE username=%s AND password_=%s"
+    #     cursor.execute(query, (username, password))
+    #     user = cursor.fetchone()
+    #     cursor.close()
+
+    #     if user:
+    #         return render_template("prompt.html")
+    #     else:
+    #         return "Invalid username or password."
+    # return render_template("index.html")
+
+@app.route("/prompt", methods=["GET", "POST"])
+def prompt():
+    return render_template("prompt.html")
+
+@app.route('/save_prompt', methods=['POST'])
+def save_prompt():
+    img = request.files.get('imageUpload')
+    prompt_text = request.form.get('promptText')
+
+    image_pil = Image.open(img.stream).convert('RGB')
+
+    sketch = img_to_sketch(image_pil)
+    # sketch.show()
+    # show_fullscreen(sketch)
+    buffer = BytesIO()
+    sketch.save(buffer, format='PNG')
+    buffer.seek(0)
+    # sketch.show()
+
+    payload = {
+        "prompt": prompt_text,
+        "controlStrength": 0.7,  # Example value, adjust as needed
+        "controlGuidanceEnd" : 0.4,  # Example value, adjust as needed
+        "seed": 68,  # Example value, adjust as needed
+        "randomizeSeed": "true"
+    }
+    files = {
+        "controlImage": ("controlImage.png", buffer, "image/png")
+    }
+    
+    # response = requests.post(COLAB_BACKEND_URL, data=payload, files=files)
+
+
+    # output_image = Image.open(BytesIO(response.content))
+    # output_image_path = os.path.join(app.config['UPLOAD_FOLDER'], "output.png")
+    # output_image.show()
+    # output_image.save(output_image_path)
+    # time.sleep(2)
+
+    return render_template("dashboard.html")
+
+    # else:
+    #     return jsonify({"error": "Failed to generate image"}), 500
+    
+
+    # return redirect(url_for('dashboard'))
+
+
+# Dashboard route (protected)
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+
+
+# Register route
+@app.route('/register')
+def register():
+    return "Register Page"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    # if response.status_code == 200:
+        # Save the generated image from the Colab backend
+    # print("Response headers:", response.headers)
+    # print("Response content-type:", response.headers.get("Content-Type"))
+    # print("Response length:", len(response.content))
+    # try:
+    #     json_data = response.json()
+    #     print("Colab JSON response:", json_data)
+    # except Exception as e:
+    #     print("Failed to parse JSON:", e)
